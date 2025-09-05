7bec08757af711dced36065a1ac702e376051542
davidpacecode <davidpace@gmail.com>
2025-09-04 22:55:32 -0500

    simplifying and trying to debug w claude

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,30 +1,35 @@
 class ChatsController < ApplicationController
   def new
-    @chat = Chat.new
-    @chat.messages.build # Build the first message
-  end
-
-  def create
-    @chat = Current.user.chats.build(chat_params)
-
-    if @chat.save
-      first_message = @chat.messages.first
-      ProcessMessageJob.perform_later(first_message) if first_message
-      redirect_to @chat
-    else
-      @message = Message.new
-      render :new, status: :unproccessable_entity
-    end
-  end
-
-  def show
-    @chat = Current.user.chats.find(params[:id])
-    @message = Message.new
-  end
-
-  private
-
-  def chat_params
-    params.require(:chat).permit(:model_id)
   end
 end
+
+
+#  def new
+#    @chat = Chat.new
+#    @chat.messages.build # Build the first message
+#  end
+#
+#  def create
+#    @chat = Current.user.chats.build(chat_params)
+#
+#    if @chat.save
+#      first_message = @chat.messages.first
+#      ProcessMessageJob.perform_later(first_message) if first_message
+#      redirect_to @chat
+#    else
+#      @message = Message.new
+#      render :new, status: :unproccessable_entity
+#    end
+#  end
+#
+#  def show
+#    @chat = Current.user.chats.find(params[:id])
+#    @message = Message.new
+#  end
+#
+#  private
+#
+#  def chat_params
+#    params.require(:chat).permit(:model_id)
+#  end
+#
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,23 +1,16 @@
 class MessagesController < ApplicationController
   def create
-    # claude said this but docs said next line @chat = Current.user.chats.find(params[:chat_id])
     @chat = Chat.find(params[:chat_id])
-    # claude said this but docs said next line @message = @chat.messages.build(message_params.merge(role: "user"))
+
+    # Create and persist the user message immediately
     @chat.create_user_message(params[:content])
 
-      # if @message.save
-      # process the LLM response asynchronously
-      # claude said this but docs said next line ProcessMessageJob.perform_later(@message)
-      ChatStreamJob.perform_later(@chat.id)
-      # clade said this but docs said below redirect_to @chat
+    ChatStreamJob.perform_later(@chat.id)
 
-      respond_to do |format|
-        format.turbo_stream { head :ok }
-        format.html { redirect_to @chat }
-      end
-    # else
-    #  render "chat/show", status: :unprocessable_entity
-    # end
+    respond_to do |format|
+      format.turbo_stream { head :ok }
+      format.html { redirect_to @chat }
+    end
   end
 
   private
